38d2cda476abb22cffc95d578413114278703c33
zarya <suhail.parmar@gmail.com>
2018-11-08 17:33:17 +0000

    Created tests for fizz

---
+++ tests/test_fizz_buzz.py
@@ -0,0 +1,23 @@
+from lib.fizz_buzz import fizz_buzz
+
+
+class TestFizzBuzz:
+    '''
+      FizzBuzzSpec
+    - Prints the numbers from 1 to 100.
+    - But for multiples of three print “Fizz”
+    - For the multiples of five print “Buzz”
+    - For numbers which are multiples of both three and five print “FizzBuzz”."
+    '''
+    fb = fizz_buzz()
+
+    def test_is_fizz_returns_true_for_muliples_of_3(self):
+
+        multiples_of_three = [3, 6, 9, 12, 30]
+        for value in multiples_of_three:
+            assert self.fb.is_fizz(value) is True
+
+    def test_is_fizz_returns_false_for_not_muliples_of_3(self):
+        not_multiples_of_three = [1, 2, 7, 11, 29]
+        for value in not_multiples_of_three:
+            assert self.fb.is_fizz(value) is False
