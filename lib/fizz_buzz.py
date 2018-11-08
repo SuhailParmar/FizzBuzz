87914cae5b9707d2e4431f73353b6b97d849a010
zarya <suhail.parmar@gmail.com>
2018-11-08 18:26:40 +0000

    Implement is_fizz_buzz

--- lib/fizz_buzz.py
+++ lib/fizz_buzz.py
@@ -2,3 +2,9 @@ class FizzBuzz:
 
     def is_fizz(self, integer):
         return (integer % 3 == 0)
+
+    def is_buzz(self, integer):
+        return (integer % 5 == 0)
+
+    def is_fizz_buzz(self, integer):
+        return (self.is_fizz(integer) and self.is_buzz(integer))
