3020cc2a3a8545efdeebfe02ae2ded81a1367f07
zarya <suhail.parmar@gmail.com>
2018-11-08 18:52:54 +0000

    Implement FizzBuzz

--- lib/fizz_buzz.py
+++ lib/fizz_buzz.py
@@ -8,3 +8,14 @@ class FizzBuzz:
 
     def is_fizz_buzz(self, integer):
         return (self.is_fizz(integer) and self.is_buzz(integer))
+
+    def run_fizz_buzz(self, max_range):
+        for integer in range(1, max_range+1):
+            if self.is_fizz_buzz(integer):
+                print('FizzBuzz')
+            elif self.is_buzz(integer):
+                print('Buzz')
+            elif self.is_fizz(integer):
+                print('Fizz')
+            else:
+                print(integer)
